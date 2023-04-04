3f2c56cfcdcec0ccf2e5ff607da33ec750d6775b
MukulDevPatel <patelmukuldev@gmail.com>
2023-04-04 17:09:08 +0530

    [Mukul Dev] Add: Mood Analyzer test program

---
+++ MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿namespace MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        private string message; //UC1
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if (message.Contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
